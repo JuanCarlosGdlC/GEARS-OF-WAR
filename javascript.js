9fc912003530793f77c68cd8cd419414585d39c6
JuanCarlosGdlC <juancarlosgarciadeleoncarrillo@gmail.com>
2022-09-07 18:03:49 -0500

    Proyecto Escolar

---
+++ javascript.js
@@ -0,0 +1,52 @@
+let pedir1=document.getElementById("pedir1");
+let pedir2=document.getElementById("pedir2");
+let pedir3=document.getElementById("pedir3");
+let pedir4=document.getElementById("pedir4");
+let pedir5=document.getElementById("pedir5");
+let pedir6=document.getElementById("pedir6");
+let pedir7=document.getElementById("pedir7");
+let pedir8=document.getElementById("pedir8");
+let pedir9=document.getElementById("pedir9");
+let pedir10=document.getElementById("pedir10");
+
+pedir1.addEventListener("click", function(){
+    confirm("Tu pedido esta siendo procesado, Gracias")
+});
+
+pedir2.addEventListener("click", function(){
+    confirm("Tu pedido esta siendo procesado, Gracias")
+});
+
+pedir3.addEventListener("click", function(){
+    confirm("Tu pedido esta siendo procesado, Gracias")
+});
+
+pedir4.addEventListener("click", function(){
+    confirm("Tu pedido esta siendo procesado, Gracias")
+});
+
+pedir5.addEventListener("click", function(){
+    confirm("Tu pedido esta siendo procesado, Gracias")
+});
+
+pedir6.addEventListener("click", function(){
+    confirm("Tu pedido esta siendo procesado, Gracias")
+});
+
+pedir7.addEventListener("click", function(){
+    confirm("Tu pedido esta siendo procesado, Gracias")
+});
+
+pedir8.addEventListener("click", function(){
+    confirm("Tu pedido esta siendo procesado, Gracias")
+});
+
+pedir9.addEventListener("click", function(){
+    confirm("Tu pedido esta siendo procesado, Gracias")
+});
+
+pedir10.addEventListener("click", function(){
+    confirm("Tu pedido esta siendo procesado, Gracias")
+});
+
+console.log("prueba conexion");
